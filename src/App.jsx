da7f36f0f3fb52f21371c7052095fe44d4844cd1
Tay Kong Yu <kongyutay@gmail.com>
2024-12-13 19:35:41 +0800

    computing values

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         setUserInput((prevUserInput) => {
             return {
                 ...userInput,
-                [inputIdentifier]: newValue,
+                [inputIdentifier]: +newValue,
             };
         });
     }
